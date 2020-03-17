b831fe00043e33ce8524c1ea545b22f367d7be0d
mingkai.wang <mingkai.wang@xiaoi.com>
2020-03-17 10:52:15 +0800

    feat: init

---
+++ src/test/java/com/michael/datastructure/DataStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.datastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
